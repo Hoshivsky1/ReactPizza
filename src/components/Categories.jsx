41cfcf35c07ae34ef5869b5ba3e9820b012a63e2
Maxim <96632620+Hoshivsky1@users.noreply.github.com>
2022-08-13 21:13:13 +0300

    Update Categories/Pizza

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,16 +1,25 @@
-const Categories = () =>  {
-    return (
-      <div class="categories">
-        <ul>
-          <li class="active">Все</li>
-          <li>Мясные</li>
-          <li>Вегетарианская</li>
-          <li>Гриль</li>
-          <li>Острые</li>
-          <li>Закрытые</li>
-        </ul>
-      </div>
+import { useState } from "react";
+
+const Categories = () => {
+	const [activeIndex, setActiveIndex] = useState(0)
+	
+	const categories = ['Всі', 'Мясні', 'Вегетерянські', 'Гриль', 'Гострі', 'Закриті']
+
+	const onClickCategory = (index) => {
+		setActiveIndex(index)
+	}
+
+	return (
+        <div className="categories">
+            <ul>
+				{categories.map(i => {
+					return(
+						<li className={activeIndex === i ? 'active': null} onClick={() => onClickCategory(i)}>{i}</li>
+					)
+				})}
+            </ul>
+        </div>
     );
-}
+};
 
 export default Categories;
